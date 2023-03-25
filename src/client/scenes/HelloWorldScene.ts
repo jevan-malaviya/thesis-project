9ec738b30d53b47e0810cc273f905e8682b75ad7
Jevan Malaviya <jevan.malaviya@gmail.com>
2023-03-25 14:06:22 -0400

    mo platforms

--- src/client/scenes/HelloWorldScene.ts
+++ src/client/scenes/HelloWorldScene.ts
@@ -5,15 +5,26 @@ export default class HelloWorldScene extends Phaser.Scene {
   private client!: Colyseus.Client;
   private player?: Phaser.GameObjects.Sprite &
     Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private currentPlayer?: Phaser.GameObjects.Sprite &
+    Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private playerEntities: { [sessionId: string]: any } = {};
   private room?: Colyseus.Room;
+
+  localRef?: Phaser.GameObjects.Rectangle;
+  remoteRef?: Phaser.GameObjects.Rectangle;
 
   inputPayload = {
     left: false,
     right: false,
     up: false,
+    tick: 0,
   };
+
+  elapsedTime = 0;
+  fixedTimeStep = 1000 / 60;
+
+  currentTick: number = 0;
 
   constructor() {
     super("hello-world");
@@ -48,7 +59,7 @@ export default class HelloWorldScene extends Phaser.Scene {
     platforms.create(50, 250, "ground");
     platforms.create(750, 220, "ground");
 
-    this.room.state.players.onAdd((player: any, sessionId: string) => {
+    this.room.state.players.onAdd = (player: any, sessionId: string) => {
       const entity = this.physics.add.sprite(player.x, player.y, "dude");
       this.physics.add.existing(entity);
       entity.setBounce(0.2);
@@ -56,19 +67,38 @@ export default class HelloWorldScene extends Phaser.Scene {
       this.physics.add.collider(entity, platforms);
       this.playerEntities[sessionId] = entity;
 
-      player.onChange(() => {
-        entity.x = player.x;
-        entity.y = player.y;
-      });
-    });
+      if (sessionId === this.room?.sessionId) {
+        this.currentPlayer = entity;
 
-    this.room.state.players.onRemove((player: any, sessionId: string) => {
+        this.localRef = this.add.rectangle(0, 0, entity.width, entity.height);
+        this.localRef.setStrokeStyle(1, 0x00ff00);
+
+        this.remoteRef = this.add.rectangle(0, 0, entity.width, entity.height);
+        this.remoteRef.setStrokeStyle(1, 0xff0000);
+
+        player.onChange = () => {
+          this.remoteRef!.x = player.x;
+          this.remoteRef!.y = player.y;
+        };
+      } else {
+        // listening for server updates
+        player.onChange = () => {
+          //
+          // we're going to LERP the positions during the render loop.
+          //
+          entity.setData("serverX", player.x);
+          entity.setData("serverY", player.y);
+        };
+      }
+    };
+
+    this.room.state.players.onRemove = (player: any, sessionId: string) => {
       const entity = this.playerEntities[sessionId];
       if (entity) {
         entity.destroy();
         delete this.playerEntities[sessionId];
       }
-    });
+    };
 
     this.anims.create({
       key: "left",
@@ -91,28 +121,59 @@ export default class HelloWorldScene extends Phaser.Scene {
     });
   }
 
-  update() {
-    if (!this.room) return;
+  update(time: number, delta: number): void {
+    // skip loop if not connected yet.
+    if (!this.currentPlayer) {
+      return;
+    }
+
+    this.elapsedTime += delta;
+    while (this.elapsedTime >= this.fixedTimeStep) {
+      this.elapsedTime -= this.fixedTimeStep;
+      this.fixedTick(time, this.fixedTimeStep);
+    }
+  }
+
+  fixedTick(time: number, delta: number) {
+    this.currentTick++;
 
     this.inputPayload.left = this.cursors.left.isDown;
     this.inputPayload.right = this.cursors.right.isDown;
     if (this.cursors.up.isDown && this.player?.body.touching.down) {
       this.inputPayload.up = this.cursors.up.isDown;
     }
-    this.room.send(0, this.inputPayload);
+    this.inputPayload.tick = this.currentTick;
+    this.room!.send(0, this.inputPayload);
 
-    if (this.cursors.left.isDown) {
-      this.player?.setVelocityX(-160);
-      this.player?.anims.play("left", true);
-    } else if (this.cursors.right.isDown) {
-      this.player?.setVelocityX(160);
-      this.player?.anims.play("right", true);
+    if (this.inputPayload.left) {
+      this.currentPlayer!.x = -160;
+      this.currentPlayer!.anims.play("left", true);
+    } else if (this.inputPayload.right) {
+      this.currentPlayer!.setVelocityX(160);
+      this.currentPlayer!.anims.play("right", true);
     } else {
-      this.player?.setVelocityX(0);
-      this.player?.anims.play("turn");
+      this.currentPlayer!.x = 0;
+      this.currentPlayer!.anims.play("turn");
     }
-    if (this.cursors.up.isDown && this.player?.body.touching.down) {
-      this.player.setVelocityY(-330);
+    if (this.inputPayload.up && this.currentPlayer!.body.touching.down) {
+      this.currentPlayer!.y = -330;
+    }
+
+    this.localRef!.x = this.currentPlayer!.x;
+    this.localRef!.y = this.currentPlayer!.y;
+
+    for (let sessionId in this.playerEntities) {
+      // interpolate all player entities
+      // (except the current player)
+      if (sessionId === this.room?.sessionId) {
+        continue;
+      }
+
+      const entity = this.playerEntities[sessionId];
+      const { serverX, serverY } = entity.data.values;
+
+      entity.x = Phaser.Math.Linear(entity.x, serverX, 0.2);
+      entity.y = Phaser.Math.Linear(entity.y, serverY, 0.2);
     }
   }
 }
