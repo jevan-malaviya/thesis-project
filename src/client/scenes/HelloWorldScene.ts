da1b71ad67cf1ca3365b2cd4998a232377df636c
Asa <habana.barroso@gmail.com>
2023-03-28 10:03:45 -0400

    Weapon selection menu done, and logic for passing data through levels

--- src/client/scenes/HelloWorldScene.ts
+++ src/client/scenes/HelloWorldScene.ts
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 import * as Colyseus from "colyseus.js";
-import WeaponPlugin from 'phaser3-weapon-plugin';
+// import WeaponPlugin from 'phaser3-weapon-plugin';
 
 export default class HelloWorldScene extends Phaser.Scene {
   private client!: Colyseus.Client;
@@ -9,8 +9,6 @@ export default class HelloWorldScene extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private playerEntities: { [sessionId: string]: any } = {};
   private room?: Colyseus.Room;
-
-  private weapon!: any;
 
 
   inputPayload = {
@@ -22,7 +20,24 @@ export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
     super("hello-world");
   }
+  
+  //Fix
+  // createWeapon(player: Phaser.GameObjects.Sprite & Phaser.Types.Physics.Arcade.SpriteWithDynamicBody){
+  //   const weapon = this.physics.add.group({
+  //     classType: Phaser.GameObjects.Image,
+  //     key: 'bullet',
+  //     active: false,
+  //     visible: false,
+  //     maxSize: 10,
+  //     defaultKey: 'bullet',
+  //   });
 
+  //   weapon.children.iterate((bullet: Phaser.GameObjects.Image) => {
+  //     bullet.setScale(0.5); // Adjust the scale of bullet
+  //   });
+
+  //   player.weapon = weapon;
+  
   init() {
     this.client = new Colyseus.Client("ws://localhost:2567");
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -37,6 +52,9 @@ export default class HelloWorldScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48,
     });
+
+    this.load.image('bullet', '../assets/bullet.png');
+    this.load.image('gun-icon', '../assets/gun.png');
     
     //Loading maps & tilesets for maps
     this.load.image('retro-tiles', '../assets/tiles/Retro-Lines-Tiles.png');
@@ -49,12 +67,28 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.load.tilemapTiledJSON('mountain-map', 'assets/tiles/MountainRange.json')
   }
 
+  //Fix
+    fireBullet(player: Phaser.GameObjects.Sprite & Phaser.Types.Physics.Arcade.SpriteWithDynamicBody) {
+    const bullet = player.weapon.get(player.x, player.y);
+    if (bullet) {
+      bullet.setActive(true);
+      bullet.setVisible(true);
+      this.physics.moveTo(bullet, this.input.x + this.cameras.main.scrollX, this.input.y + this.cameras.main.scrollY, 600); // 600 is the bullet speed
+      this.physics.add.collider(bullet, platforms, () => {
+        bullet.setActive(false);
+        bullet.setVisible(false);
+        bullet.setPosition(-100, -100);
+      });
+    }
+  }
+
   async create() {
     this.room = await this.client.joinOrCreate("my_room");
 
     console.log(this.room.sessionId);
 
     this.add.image(400, 300, "sky");
+
 
     const platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
@@ -70,10 +104,52 @@ export default class HelloWorldScene extends Phaser.Scene {
       this.physics.add.collider(entity, platforms);
       this.playerEntities[sessionId] = entity;
 
-      player.onChange(() => {
-        entity.x = player.x;
-        entity.y = player.y;
-      });
+
+      //Fix
+      // this.createWeapon(entity);
+
+      //Health & Combat
+
+      // player.health = 100;
+
+      // this.physics.add.collider(entity, enemyEntity, () => {
+      //   player.health -=10;
+
+      //   if(player.health <=0){
+      //     this.scene.start('selection-menu')
+      //   }
+      // })
+
+      // player.onChange(() => {
+      //   entity.x = player.x;
+      //   entity.y = player.y;
+      // });
+
+      this.physics.add.collider(entity, this.player?.weapon, () => {
+        player.health -= 10;
+
+        if(player.health <= 0){
+          player.lives--;
+
+          if(player.lives > 0) {
+            player.health = 100;
+
+            //Storing scene key to check in selection menu where the player needs to be after selecting
+            this.registry.set('previousSceneKey', this.scene.key);
+            this.scene.start('selection-menu');
+          } else {
+            this.scene.start('Farmlevel',{
+              playerData: {
+                x: player.x,
+                y: player.y,
+                health: player.health,
+                lives: player.lives,
+              },
+            });
+          }
+        }
+      })
+      
     });
 
     this.room.state.players.onRemove((player: any, sessionId: string) => {
@@ -117,10 +193,16 @@ export default class HelloWorldScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+
+    this.input.on('pointerdown', () => {
+      if (this.player) {
+        this.fireBullet(this.player);
+      }
+    });
     
   
-    this.scene.start('FarmLevel');
   }
+
 
   update() {
     if (!this.room) return;
@@ -131,6 +213,10 @@ export default class HelloWorldScene extends Phaser.Scene {
       this.inputPayload.up = this.cursors.up.isDown;
     }
     this.room.send(0, this.inputPayload);
+
+    if (this.player) {
+      this.player.weaponKey = this.registry.get("selectedWeapon");
+    }
 
     if (this.cursors.left.isDown) {
       this.player?.setVelocityX(-160);
@@ -144,6 +230,19 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
     if (this.cursors.up.isDown && this.player?.body.touching.down) {
       this.player.setVelocityY(-330);
+    }
+
+    //Fix
+    if (this.player) {
+      this.player.weapon.children.iterate((bullet: Phaser.GameObjects.Image) => {
+        if (bullet.active) {
+          if (bullet.x < 0 || bullet.x > this.physics.world.bounds.width || bullet.y < 0 || bullet.y > this.physics.world.bounds.height) {
+            bullet.setActive(false);
+            bullet.setVisible(false);
+            bullet.setPosition(-100, -100);
+          }
+        }
+      });
     }
   }
 }
